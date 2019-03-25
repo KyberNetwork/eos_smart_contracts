28280d110a6d56742ac5759657e94bf4046b17e3
Tal Baneth <tal@kyber.network>
2019-03-25 08:27:01 +0200

    Add doxygen documentation

--- contracts/Network/Network.hpp
+++ contracts/Network/Network.hpp
@@ -60,30 +60,109 @@ CONTRACT Network : public contract {
         typedef eosio::multi_index<"tokenstats"_n, tokenstats> tokenstats_type;
         typedef eosio::singleton<"rate"_n, rate> rate_type;
 
+        /**
+         * Init the contract.
+         * Should be called right after deploying the contract.
+         * Can only be called once, and only by the network account authority (owner).
+         *
+         * @param admin - the only account that can deposit/withdraw tokens,
+         * and configure the network contract.
+         * @param eos_contract - account of EOS native token, usually eosio.token.
+         * @param enable - whether to initiate the network in an operating state,
+         * or otherwise wait for a setenable operation.
+         */
         ACTION init(name admin, name eos_contract, bool enable);
 
+        /**
+         * Change the admin account.
+         * Can only be called by the admin.
+         *
+         * @param admin - the new admin account.
+         */
         ACTION setadmin(name admin);
 
+        /**
+         * Enable or disable the network.
+         * Can only be called by the admin.
+         *
+         * @param enable - enable or disable.
+         */
         ACTION setenable(bool enable);
 
+        /**
+         * Add/Remove a reserve to/from the network.
+         * Can only be called by the admin.
+         *
+         * @param reserve - account of the reserve contract.
+         * @param add - add or remove.
+         */
         ACTION addreserve(name reserve, bool add);
 
+        /**
+        * List/Unlist a trade pair on the network.
+        * Can only be called by the admin.
+        *
+        * @param reserve - account of the reserve where the pair is added/removed to/from.
+        * @param token_symbol - symbol of the added/removed token, which will be paired with EOS.
+        * @param token_contract - account contract implementing the added/removed token.
+        * @param add - add or remove the pair.
+        */
         ACTION listpairres(name reserve, symbol token_symbol, name token_contract, bool add);
 
+        /**
+         * Withdraw funds from the network account. Can only be called by the admin.
+         *
+         * @param to - account to withdraw to.
+         * @param quantity - asset to withdraw.
+         * @param dest_contract - account implementing the withdrawn token.
+         * @param memo - optional memo to be added to the end withdraw transfer operation.
+         */
         ACTION withdraw(name to, asset quantity, name dest_contract, string memo);
 
-        /* for on chain integration only, result written to "rate" singleton. */
+        /**
+         * Get expected rate for a specific pair.
+         * Result is written to the “rate” table.
+         * Should only be used for on chain integration.
+         * Only in such on-chain contracts integration the table can be read in atomic manner.
+         *
+         * @param src - src asset in the pair to query rate for.
+         * @param dest_symbol - destination token symbol for the rate query.
+         */
         ACTION getexprate(asset src, symbol dest_symbol);
 
-        /* internal */
+        /*
+         * The following functions are internal actions.
+         * They are purposed to only be called internally by the network contract.
+         * We implement these as separate actions (and not regular functions/logic),
+         * since when calling a preceding action, the only way to assure their logic
+         * would happen after that action is to make them a separate action on their own.
+         */
+
+        /** internal */
         ACTION storeexprate(asset src, symbol dest_symbol);
 
-        /* internal */
+        /** internal */
         ACTION trade1(trade_info info);
 
-        /* internal */
+        /** internal */
         ACTION trade2(name reserve, trade_info info, asset src, asset dest, asset balance_pre);
 
+        /**
+         * Notification handler for transfer events from/to this contract.
+         * Before init() is called anyone can deposit to the contract.
+         * After init() is called only the admin can deposit.
+         * At that stage any other transfer to the contract is regarded as a trade attempt,
+         * and expected to have a valid memo for a trade.
+         * Note that the memo's min conversion rate parameter is the way for
+         * the user to ensure that the actual rate for the trade is not less
+         * than what he expects.
+         *
+         * @param from - sender.
+         * @param to - recipient, this contract.
+         * @param quantity - sent asset.
+         * @param memo - Expected as “<dest symbol>,<dest contract>,<recipient account>,<min conversion rate>”
+         * For example: "4 KARMA,therealkarma,bob111111111,7200.0000"
+         */
         void transfer(name from, name to, asset quantity, string memo);
 
     private:
--- contracts/Reserve/AmmReserve/AmmReserve.hpp
+++ contracts/Reserve/AmmReserve/AmmReserve.hpp
@@ -42,6 +42,21 @@ CONTRACT AmmReserve : public contract {
         typedef eosio::singleton<"params"_n, params> params_type;
         typedef eosio::singleton<"rate"_n, rate> rate_type;
 
+        /**
+         * Init the reserve.
+         * Should be called right after deploying the contract.
+         * Can only be called once, and only by the reserve account authority.
+         *
+         * @param admin - the only account that can deposit/withdraw tokens,
+         * and configure the reserve contract.
+         * @param network_contract - contract of the network the reserve is listed on.
+         * Only the network contract is allowed to trade through the reserve.
+         * @param token_symbol - the symbol of the token traded on the reserve.
+         * @param token_contract - the contract implementing the token traded on the reserve.
+         * @param eos_contract - account of eos native token, usually eosio.token.
+         * @param enable_trade - whether to initiate the reserve in an operating state,
+         * or otherwise wait for a setenable operation.
+         */
         ACTION init(name    admin,
                     name    network_contract,
                     symbol  token_symbol,
@@ -49,8 +64,30 @@ CONTRACT AmmReserve : public contract {
                     name    eos_contract,
                     bool    enable_trade);
 
+        /**
+         * Set reserve parameters quickly, using default values.
+         * Can only be called by the reserve admin.
+         * Liquidity rate (r) will be auto calculated according to the contract’s EOS amount.
+         * Supported price movements will be half to twice of the initial price.
+         * No cap restrictions per single trade will be enforced.
+         * Profit percent will be set to 0.
+         *
+         * @param p - initial token to EOS sell price.
+         */
         ACTION quickset(double p);
 
+        /**
+        * Set reserve parameters.
+        * Can only be called by the reserve admin.
+        *
+        * @param r - liquidity rate.
+        * @param p_min - minimum supported price, in token sell convention.
+        * @param max_eos_cap_buy - maximum single buy amount in EOS units.
+        * @param max_eos_cap_sell - maximum single sell amount in EOS units.
+        * @param profit_percent - percent of reserve tokens profit per trade.
+        * @param max_sell_rate - maximum rate allowed, in token sell convention.
+        * @param min_sell_rate - minimum rate allowed, in token sell convention.
+        */
         ACTION setparams(double r,
                          double p_min,
                          asset  max_eos_cap_buy,
@@ -59,18 +96,68 @@ CONTRACT AmmReserve : public contract {
                          double max_sell_rate,
                          double min_sell_rate);
 
+        /**
+         * Change the admin account.
+         * Can only be called by the reserve admin.
+         *
+         * @param admin - the new admin account.
+         */
         ACTION setadmin(name admin);
 
+        /**
+         * Change the registered network contract.
+         * Can only be called by the reserve admin.
+         * Only the registered network account can send trades to the reserve.
+         *
+         * @param network_contract - the new network contract.
+         */
         ACTION setnetwork(name network_contract);
 
+        /**
+         * Enable or disable the reserve.
+         * Can only be called by the reserve admin.
+         * When disabled, both trade and get conversion rate are disabled.
+         *
+         * @param enable - enable or disable.
+         */
         ACTION setenable(bool enable);
 
+        /**
+         * Reset the reserve’s profit counter.
+         * Can only be called by the reserve admin.
+         */
         ACTION resetprofit();
 
+        /**
+         * Get conversion rate.
+         * Can only be called by the network contract, as registered in the reserve.
+         * Result will be written to the trade table.
+         *
+         * @param src - src asset for the rate query. Can be either EOS or the reserve’s token.
+         */
         ACTION getconvrate(asset src);
 
+        /* Withdraw funds from the reserve account.
+         * Can only be called by the reserve admin.
+         *
+         * @param to - account to withdraw to.
+         * @param quantity - asset to withdraw.
+         * @param dest_contract - account implementing the withdrawn token.
+         * @param memo - optional memo to be added to the withdraw end transfer operation.
+         */
         ACTION withdraw(name to, asset quantity, name dest_contract, string memo);
 
+        /* Notification handler for transfer events from/to this contract.
+         * Before init() is called anyone can deposit to the contract.
+         * After init() is called only the contract admin can deposit.
+         * Any other transfer to the contract is regarded as a trade attempt.
+         * A trade is expected to come from the network account and have a valid memo.
+         *
+         * @param name - sender.
+         * @param to - recipient, this contract.
+         * @quantity - sent asset
+         * @memo - for trades expected as “<dest account>”. For example: "bob111111111".
+         */
         void transfer(name from, name to, asset quantity, string memo);
 
     private:
