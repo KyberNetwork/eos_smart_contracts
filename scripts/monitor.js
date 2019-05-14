7e829dffcfcba3df4f86b19dfca847ff2f92c6f6
Tal Baneth <tal@kyber.network>
2019-05-14 21:29:54 +0300

    add compare functionality to monitor script

--- scripts/monitor.js
+++ scripts/monitor.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const Eos = require('eosjs')
 const networkServices = require('../scripts/services/networkServices')
 
@@ -8,7 +9,7 @@ const eos = Eos({
     chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
 });
 
-async function main() {
+async function status() {
 
     dict = { "network_tables" : {}, "reserves_stats" : {} }
     summary = { "reserves_stats" : {}, "tokenstats" : {} }
@@ -54,7 +55,59 @@ async function main() {
     }
 
     summary["tokenstats"] = dict["network_tables"]["tokenstats"]
-    console.log(JSON.stringify(summary, null, 4))
+    return summary
+}
+
+async function compare(data1, data2) {
+
+    tokenstats1 = data1["tokenstats"]["rows"]
+    tokenstats2 = data2["tokenstats"]["rows"]
+    
+    diffDict = {}
+    
+    for (i in tokenstats1) {
+        symbol1 = tokenstats1[i]["token_counter"].split(" ")[1]
+        for (j in tokenstats2) {
+            symbol2 = tokenstats2[j]["token_counter"].split(" ")[1]
+            if (symbol1 == symbol2) {
+                eos1 = tokenstats1[i]["eos_counter"].split(" ")[0]
+                eos2 = tokenstats2[j]["eos_counter"].split(" ")[0]
+                diff = parseFloat(eos2) - parseFloat(eos1)
+                diffDict[symbol1] = diff
+                break
+            }
+        }
+    }
+    return diffDict
+}
+
+const { mode, file1, file2 } = require('yargs')
+.usage('Usage: $0 --mode [mode] --file1 [file1] --file2 [file2]')
+.demandOption(['mode'])
+.argv;
+
+async function main() {
+    if (mode == "status") {
+        output = await status();
+    } else if (mode == "compare_status") {
+        status = await status()
+        oldData = JSON.parse(fs.readFileSync(file1, 'utf8'));
+        output = await compare(oldData, status)
+    } else if (mode == "compare_files") {
+        oldData1 = JSON.parse(fs.readFileSync(file1, 'utf8'));
+        oldData2 = JSON.parse(fs.readFileSync(file2, 'utf8'));
+        output = await compare(oldData1, oldData2)
+    } else {
+        throw "invalid mode"
+    }
+    console.log(JSON.stringify(output, null, 4))
 }
 
 main()
+
+// examples:
+// node scripts/monitor.js --mode status > yolo_stats_$(date +"%Y_%m_%d_%H_%M").log
+// node scripts/monitor.js --mode compare_status --file1 yolo_stats_2019_05_14_11_27.log 
+// node scripts/monitor.js --mode compare_files  --file1 yolo_stats_2019_05_14_11_27.log --file2 yolo_stats_2019_05_14_18_31.log
+
+
