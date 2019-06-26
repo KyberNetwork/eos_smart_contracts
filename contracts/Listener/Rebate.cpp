ee5a57abf936dd7d7ff2ece3ba6951223c517eaa
Tal Baneth <tal@kyber.network>
2019-06-26 16:17:21 +0300

    rebate contract - initial implementation

---
+++ contracts/Listener/Rebate.cpp
@@ -0,0 +1,108 @@
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/asset.hpp>
+#include "../Common/common.hpp"
+
+using namespace eosio;
+
+#define SECONDS_PER_WEEK 604800
+#define WEEKS_TO_RUN 5
+
+CONTRACT Rebate : public contract {
+    public:
+        using contract::contract;
+
+        TABLE state {
+            name        network_contract;
+            name        token_contract;
+            asset       default_reward;
+            uint64_t    start_time_seconds;
+        };
+
+        TABLE userreward {
+            name            user;
+            vector<asset>   reward_per_week;
+            uint64_t        primary_key() const { return user.value; }
+        };
+
+        typedef eosio::singleton<"state"_n, state> state_type;
+        typedef eosio::multi_index<"userreward"_n, userreward> userreward_type;
+
+        ACTION config(name  network_contract,
+                      name  token_contract,
+                      asset default_reward) {
+            eosio_assert(is_account(network_contract), "network contract does not exist");
+            eosio_assert(is_account(token_contract), "token contract does not exist");
+
+            require_auth(_self);
+
+            // current_time() returns time in microseconds (rounded to block time)
+            uint64_t start_time_seconds = current_time() / 1000000;
+
+            state_type state_inst(_self, _self.value);
+            state new_state = {
+                    network_contract,
+                    token_contract,
+                    default_reward,
+                    start_time_seconds
+            };
+            state_inst.set(new_state, _self);
+        }
+
+        ACTION posttrade(asset src, asset dest, name reserve, name sender) {
+            state_type state_inst(_self, _self.value);
+            if (!state_inst.exists()) return;
+            auto state = state_inst.get();
+            require_auth(state.network_contract);
+
+            uint64_t current_time_seconds = current_time() / 1000000;
+            uint64_t week_index = (current_time_seconds - state.start_time_seconds) / SECONDS_PER_WEEK;
+
+            userreward_type userreward_inst(_self, _self.value);
+            auto itr = userreward_inst.find(sender.value);
+            bool user_exists = (itr != userreward_inst.end());
+
+            asset rewarded_this_week = asset(0, state.default_reward.symbol);
+            asset rewarded_last_week = asset(0, state.default_reward.symbol);
+            if (user_exists) {
+                rewarded_this_week = itr->reward_per_week[week_index];
+                if (week_index > 0) rewarded_last_week = itr->reward_per_week[week_index - 1];
+            }
+
+            if (rewarded_this_week.amount > 0 || week_index > (WEEKS_TO_RUN - 1)) return;
+
+            asset reward = state.default_reward + rewarded_last_week;
+            if (!user_exists) {
+                userreward_inst.emplace(_self, [&](auto& s) {
+                   s.user = sender;
+                   for( int i = 0; i < WEEKS_TO_RUN; i++) {
+                       s.reward_per_week.push_back(asset(0, state.default_reward.symbol));
+                   }
+                   s.reward_per_week[week_index] = reward;
+                });
+            } else {
+                userreward_inst.modify(itr, _self, [&](auto& s) {
+                    s.user = sender;
+                    s.reward_per_week[week_index] = reward;
+                });
+            }
+
+            asset balance = get_balance(_self, state.token_contract, state.default_reward.symbol);
+
+            if (balance >= reward) {
+                async_pay(_self, sender, reward, state.token_contract, "yolo rebate");
+            }
+
+            return;
+        }
+};
+
+extern "C" {
+    [[noreturn]] void apply(uint64_t receiver, uint64_t code, uint64_t action) {
+        if (code == receiver) {
+            switch (action) {
+                EOSIO_DISPATCH_HELPER( Rebate, (config)(posttrade))
+            }
+        }
+        eosio_exit(0);
+    }
+}
---
+++ tests/rebate.js
@@ -0,0 +1,180 @@
+const fs = require('fs')
+const Eos = require('eosjs')
+const BigNumber = require('bignumber.js');
+const path = require('path');
+const should = require('chai').should();
+const assert = require('assert');
+
+const {ensureContractAssertionError, snooze, getUserBalance,
+       renouncePermToOnlyCode, addCodeToPerm, roundDown} = require('./utils');
+const networkServices = require('../scripts/services/networkServices')
+
+const AMOUNT_PRECISON = 0.0001
+const RATE_PRECISON =   0.00000001
+
+/* Assign keypairs. to accounts. Use unique name prefixes to prevent collisions between test modules. */
+const keyPairArray = JSON.parse(fs.readFileSync("tests/keys.json"))
+const tokenData =    {account: "rebtoken",   publicKey: keyPairArray[0][0], privateKey: keyPairArray[0][1]}
+const reserveData =  {account: "rebreserve", publicKey: keyPairArray[1][0], privateKey: keyPairArray[1][1]}
+const networkData =  {account: "rebnetwork", publicKey: keyPairArray[2][0], privateKey: keyPairArray[2][1]}
+const rebateData =   {account: "reblistener",publicKey: keyPairArray[3][0], privateKey: keyPairArray[3][1]}
+const aliceData =    {account: "rebalice",   publicKey: keyPairArray[4][0], privateKey: keyPairArray[4][1]}
+const walletData =   {account: "rebwallet",  publicKey: keyPairArray[5][0], privateKey: keyPairArray[5][1]}
+
+const systemData =  {account: "eosio", publicKey: "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV", privateKey: "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"}
+
+/* create eos handler objects */
+systemData.eos = Eos({ keyProvider: systemData.privateKey /* , verbose: 'false' */})
+tokenData.eos = Eos({ keyProvider: tokenData.privateKey /* , verbose: 'false' */})
+reserveData.eos = Eos({ keyProvider: reserveData.privateKey /* , verbose: 'false' */})
+networkData.eos = Eos({ keyProvider: networkData.privateKey /* , verbose: 'false' */})
+rebateData.eos = Eos({ keyProvider: rebateData.privateKey /* , verbose: 'false' */})
+aliceData.eos = Eos({ keyProvider: aliceData.privateKey /* , verbose: 'false' */})
+walletData.eos = Eos({ keyProvider: walletData.privateKey /* , verbose: 'false' */})
+
+let defaultParams
+
+describe(path.basename(__filename), function () {
+before("setup accounts, contracts and initial funds", async () => {
+    /* create accounts */
+    await systemData.eos.transaction(tr => {tr.newaccount({creator: "eosio", name:tokenData.account, owner: tokenData.publicKey, active: tokenData.publicKey})});
+    await systemData.eos.transaction(tr => {tr.newaccount({creator: "eosio", name:reserveData.account, owner: reserveData.publicKey, active: reserveData.publicKey})});
+    await systemData.eos.transaction(tr => {tr.newaccount({creator: "eosio", name:networkData.account, owner: networkData.publicKey, active: networkData.publicKey})});
+    await systemData.eos.transaction(tr => {tr.newaccount({creator: "eosio", name:rebateData.account, owner: rebateData.publicKey, active: rebateData.publicKey})});
+    await systemData.eos.transaction(tr => {tr.newaccount({creator: "eosio", name:aliceData.account, owner: aliceData.publicKey, active: aliceData.publicKey})});
+    await systemData.eos.transaction(tr => {tr.newaccount({creator: "eosio", name:walletData.account, owner: walletData.publicKey, active: walletData.publicKey})});
+
+    /* deploy contracts */
+    await tokenData.eos.setcode(tokenData.account, 0, 0, fs.readFileSync(`contracts/Mock/Token/Token.wasm`));
+    await tokenData.eos.setabi(tokenData.account, JSON.parse(fs.readFileSync(`contracts/Mock/Token/Token.abi`)))
+    await reserveData.eos.setcode(reserveData.account, 0, 0, fs.readFileSync(`contracts/Reserve/AmmReserve/AmmReserve.wasm`));
+    await reserveData.eos.setabi(reserveData.account, JSON.parse(fs.readFileSync(`contracts/Reserve/AmmReserve/AmmReserve.abi`)))
+    await networkData.eos.setcode(networkData.account, 0, 0, fs.readFileSync(`contracts/Network/Network.wasm`));
+    await networkData.eos.setabi(networkData.account, JSON.parse(fs.readFileSync(`contracts/Network/Network.abi`)))
+    await rebateData.eos.setcode(rebateData.account, 0, 0, fs.readFileSync(`contracts/Listener/Rebate.wasm`));
+    await rebateData.eos.setabi(rebateData.account, JSON.parse(fs.readFileSync(`contracts/Listener/Rebate.abi`)))
+
+    /* create contract objects */
+    networkAsNetwork = await networkData.eos.contract(networkData.account);
+    networkAsAlice = await aliceData.eos.contract(networkData.account);
+    reserve = await reserveData.eos.contract(reserveData.account);
+    rebate = await rebateData.eos.contract(rebateData.account);
+
+    /* spread initial funds */
+    await tokenData.eos.transaction(tokenData.account, myaccount => {
+        myaccount.create(tokenData.account, '1000000000.0000 TOK', {authorization: tokenData.account})
+        myaccount.issue(reserveData.account, '10000.0000 TOK', 'deposit', {authorization: tokenData.account})
+        myaccount.issue(aliceData.account, '100.0000 TOK', 'deposit', {authorization: tokenData.account})
+        myaccount.issue(rebateData.account, '100.0000 TOK', 'deposit', {authorization: tokenData.account})
+    })
+
+    /* spread rebate funds */
+    await tokenData.eos.transaction(tokenData.account, myaccount => {
+        myaccount.create(tokenData.account, '1000000000.0000 TOKA', {authorization: tokenData.account})
+        myaccount.issue(rebateData.account, '100.0000 TOKA', 'deposit', {authorization: tokenData.account})
+    })
+
+    await tokenData.eos.transaction(tokenData.account, myaccount => {
+        myaccount.create(tokenData.account, '1000000000.0000 EOS', {authorization: tokenData.account})
+        myaccount.issue(networkData.account, '100.0000 EOS', 'deposit', {authorization: tokenData.account})
+        myaccount.issue(rebateData.account, '10000.0000 EOS', 'deposit', {authorization: tokenData.account})
+        myaccount.issue(reserveData.account, '69.3000 EOS', 'deposit', {authorization: tokenData.account})
+        myaccount.issue(aliceData.account, '100.0000 EOS', 'deposit', {authorization: tokenData.account})
+    })
+
+    /* init reserves, setparams */
+    await reserve.init({
+        token_symbol: "4,TOK",
+        admin: reserveData.account,
+        network_contract: networkData.account,
+        token_contract: tokenData.account,
+        eos_contract: tokenData.account,
+        enable_trade: 1,
+        },{authorization: `${reserveData.account}@active`});
+
+
+    /*set reserve params */
+    defaultParams = {
+        r: "0.01",
+        p_min: "0.05",
+        max_eos_cap_buy: "20.0000 EOS",
+        max_eos_cap_sell: "20.0000 EOS",
+        profit_percent: "0.25",
+        ram_fee: "0.0",
+        max_sell_rate: "0.5555",
+        min_sell_rate: "0.00000555",
+        fee_wallet: walletData.account
+    }
+
+    await reserve.setparams(defaultParams,{authorization: `${reserveData.account}@active`});
+    /* network configurations */
+
+    /* init network */
+    await networkAsNetwork.init({admin:networkData.account, eos_contract:tokenData.account, listener:rebateData.account, enable:1},{authorization: `${networkData.account}@active`});
+
+    await networkAsNetwork.addreserve({reserve:reserveData.account, add:1},{authorization: `${networkData.account}@active`});
+    await networkAsNetwork.listpairres({add: 1, reserve:reserveData.account, token_symbol:"4,TOK", token_contract:tokenData.account},{authorization: `${networkData.account}@active`});
+
+    /* config rebate */
+    await rebate.config({network_contract:networkData.account,
+                         token_contract:tokenData.account,
+                         default_reward:"1.0000 TOKA"},
+                        {authorization: `${rebateData.account}@active`})
+
+    /* renounce reserve control */
+    addCodeToPerm(networkData.eos, networkData.account);
+    addCodeToPerm(rebateData.eos, rebateData.account);
+    addCodeToPerm(reserveData.eos, reserveData.account);
+})
+
+describe('in first period', () => {
+    it('rebate is returned on first trade', async function() {
+        const tokenBefore = await getUserBalance({account:aliceData.account, symbol:'TOKA', tokenContract:tokenData.account, eos:aliceData.eos})
+
+        const token = await aliceData.eos.contract(tokenData.account);
+        await token.transfer({
+            from:aliceData.account,
+            to:networkData.account,
+            quantity:"5.0000 EOS",
+            memo:"4 TOK," + tokenData.account + ",0.000000"},
+            {authorization: [`${aliceData.account}@active`]});
+        
+        const tokenAfter = await getUserBalance({account:aliceData.account, symbol:'TOKA', tokenContract:tokenData.account, eos:aliceData.eos})
+        balanceChange =  tokenAfter - tokenBefore 
+        assert.equal(balanceChange, 1);
+    })
+    it('rebate is not returned on second trade in same period', async function() {
+        const tokenBefore = await getUserBalance({account:aliceData.account, symbol:'TOKA', tokenContract:tokenData.account, eos:aliceData.eos})
+
+        const token = await aliceData.eos.contract(tokenData.account);
+        await token.transfer({
+            from:aliceData.account,
+            to:networkData.account,
+            quantity:"3.0000 EOS",
+            memo:"4 TOK," + tokenData.account + ",0.000000"},
+            {authorization: [`${aliceData.account}@active`]});
+        
+        const tokenAfter = await getUserBalance({account:aliceData.account, symbol:'TOKA', tokenContract:tokenData.account, eos:aliceData.eos})
+        balanceChange =  tokenAfter - tokenBefore 
+        assert.equal(balanceChange, 0);
+    })
+});
+
+describe('in second period', () => {
+    xit('double rebate is returned if traded in first and second period', async function(){});
+    xit('regular rebate is returned if traded only in second period', async function(){});
+    xit('rebate is not returned on second trade in same period', async function() {});
+});
+
+describe('in third period', () => {
+    xit('triple rebate is returned if traded in first, second and third period', async function(){});
+    xit('regular rebate is returned if traded only in first period', async function(){});
+    xit('double rebate is returned if traded only in second period', async function(){});
+    xit('rebate is not returned on second trade in same period', async function() {});
+});
+
+describe('in 6th period', () => {
+    xit('no rebate is returned', async function(){});
+});
+
+});
